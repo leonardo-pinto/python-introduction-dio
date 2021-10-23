b5f6f1b116cad15a59bc55113832aa7e1ae86277
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-10-23 20:45:21 -0300

    aula05

---
+++ 05-arrays/05-arrays.py
@@ -0,0 +1,35 @@
+## sort an array
+
+array = [10, 3, 15, 26, 24]
+
+array.sort()
+print(array)
+array.reverse()
+print(array)
+print(len(array))
+# find if a specific number is present in an array
+# array = [1, 2, 4, 5]
+# num = 3
+#
+# if num in array:
+#     print('{} is in the array!'.format((num)))
+# else:
+#     array.append((num))
+#
+# array.pop(0)
+#
+# print(array)
+
+# else:
+#     print('{} is not in the array!'.format((num)))
+
+## sum numbers is an array
+
+# array = [10, 54, 36, 74]
+# sum = 0
+#
+# for index in array:
+#     sum = sum + index
+#
+# print(sum)
+# print(max(array))
